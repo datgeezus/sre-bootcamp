b86ef0d8cbf56193c32b26f8570e7e62d4c466cd
Jesus Sanchez <jesus.san.sev@gmail.com>
2023-02-21 14:19:23 -0500

    am refactor for testing

--- auth_api/java/auth_api/src/main/java/com/wizeline/Methods.java
+++ auth_api/java/auth_api/src/main/java/com/wizeline/Methods.java
@@ -14,16 +14,8 @@ import java.util.*;
 
 public class Methods {
 
-  private static Optional<String> getRole(String userName, String password) {
-      String dbName = System.getenv("DB_NAME");
-      String dbUsername = System.getenv("DB_USERNAME");
-      String dbPassword = System.getenv("DB_PASSWORD");
-      String hostName = System.getenv("DB_HOSTNAME");
-      String port = System.getenv("DB_PORT");
-      String url = String.format(
-              "jdbc:mysql://%s:%s/%s?user=%s&password=%s?characterEncoding=utf8",
-              hostName, port, dbName, dbUsername, dbPassword);
-    try (Connection connection = DriverManager.getConnection(url); Statement statement = connection.createStatement();) {
+  private static Optional<String> getRole(Connection connection, String userName, String password) {
+    try (Statement statement = connection.createStatement();) {
       String query = String
               .format(
                       "SELECT user, role FROM users WHERE username = %s AND password = SHA2(CONCAT(%s, salt, 512))",
@@ -41,15 +33,45 @@ public class Methods {
     return Optional.empty();
   }
 
+  private static Connection getConnection() throws SQLException {
+    String dbName = System.getenv("DB_NAME");
+    String dbUsername = System.getenv("DB_USERNAME");
+    String dbPassword = System.getenv("DB_PASSWORD");
+    String hostName = System.getenv("DB_HOSTNAME");
+    String port = System.getenv("DB_PORT");
+    String url = String.format(
+            "jdbc:mysql://%s:%s/%s?user=%s&password=%s?characterEncoding=utf8",
+            hostName, port, dbName, dbUsername, dbPassword);
+    return DriverManager.getConnection(url);
+  }
+
   public static String generateToken(String username, String password) {
-    Optional<String> roleOpt = getRole(username, password);
-    return roleOpt.map(Methods::createJWT).orElse("");
+    return Optional.ofNullable(System.getenv("SECRET"))
+            .map(secret -> generateToken(username, password, secret))
+            .orElse("");
+  }
+
+  public static String generateToken(String username, String password, String secret) {
+//    try (Connection connection = getConnection()) {
+//      Optional<String> roleOpt = getRole(connection, username, password);
+      Optional<String> roleOpt = Optional.of("admin");
+      return roleOpt.map(role -> createJWT(role, secret)).orElse("");
+//    } catch (SQLException e) {
+//      e.printStackTrace();
+//    }
+//
+//    return "";
   }
 
   public static String accessData(String authorization){
+    return Optional.ofNullable(System.getenv("SECRET"))
+            .map(secret -> accessData(authorization, secret))
+            .orElse("");
+  }
 
+  public static String accessData(String authorization, String secret){
     try {
-      var claims = decodeJWT(authorization);
+      var claims = decodeJWT(authorization, secret);
       System.out.print(claims);
       return "You are under protected data";
     }
@@ -60,9 +82,9 @@ public class Methods {
     return "test";
   }
 
-  private static Jws<Claims> decodeJWT(String jwt) {
-    String secret = System.getenv("SECRET");
-    //SecretKey key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secret));
+  private static Jws<Claims> decodeJWT(String jwt, String secret) {
+//    SecretKey key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secret));
+//    SecretKey key = Keys.hmacShaKeyFor(Base64.getDecoder().decode(secret));
     SecretKey key = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
     return Jwts.parserBuilder()
             .setSigningKey(key)
@@ -70,14 +92,13 @@ public class Methods {
             .parseClaimsJws(jwt);
   }
 
-  private static String createJWT(String role) {
-    String secret = System.getenv("SECRET");
-    //SecretKey key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secret));
+  private static String createJWT(String role, String secret) {
+//    SecretKey key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secret.getBytes(StandardCharsets.UTF_8)));
+//    SecretKey key = Keys.hmacShaKeyFor(Base64.getDecoder().decode(secret.getBytes(StandardCharsets.UTF_8)));
     SecretKey key = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
-
     return Jwts.builder()
-            .claim("role", role)
             .setHeaderParams(Map.of("typ", "JWT"))
+            .claim("role", role)
             .signWith(key, SignatureAlgorithm.HS256)
             .compact();
   }
--- auth_api/java/auth_api/src/test/java/com/wizeline/MethodsTest.java
+++ auth_api/java/auth_api/src/test/java/com/wizeline/MethodsTest.java
@@ -4,14 +4,16 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class MethodsTest {
+    private static final String EXPECTED_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYWRtaW4ifQ.StuYX978pQGnCeeaj2E1yBYwQvZIodyDTCJWXdsxBGI";
+    private static final String TEST_SECRET = "my2w7wjd7yXF64FIADfJxNs1oupTGAuW";
 
     @Test
     void generateToken() {
-        Assertions.assertEquals("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYWRtaW4ifQ.StuYX978pQGnCeeaj2E1yBYwQvZIodyDTCJWXdsxBGI", Methods.generateToken("admin", "secret"));
+        Assertions.assertEquals(EXPECTED_TOKEN, Methods.generateToken("admin", "secret", TEST_SECRET));
     }
 
     @Test
     void accessData() {
-        Assertions.assertEquals("You are under protected data", Methods.accessData("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYWRtaW4ifQ.StuYX978pQGnCeeaj2E1yBYwQvZIodyDTCJWXdsxBGI"));
+        Assertions.assertEquals("You are under protected data", Methods.accessData(EXPECTED_TOKEN, TEST_SECRET));
     }
 }
